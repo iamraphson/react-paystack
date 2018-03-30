6d06258ab34beea6d62175d04bbad50b88edaffb
Orinami Olatunji <orinamiolatunji@gmail.com>
2018-03-30 10:47:26 +0100

    Update Paystack.js

--- src/PayStack.js
+++ src/PayStack.js
@@ -19,6 +19,11 @@ class PayStack extends Component {
     }
   }
   
+  
+   componentDidMount() {
+     this.props.embed ? this.payWithPaystack(): null
+   }
+  
   payWithPaystack = () => {
     let paystackOptions = {
       key: this.props.paystackkey,
