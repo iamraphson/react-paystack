c1078f95b2f289a3f5ddcddb52fe793fe59577e8
ceofred <johnsonmessilo19@gmail.com>
2019-07-22 12:51:54 +0100

    convert to hooks

--- src/PayStack.js
+++ src/PayStack.js
@@ -1,41 +1,56 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment,useEffect,useState } from "react";
 import PropTypes from "prop-types";
 
-class PayStack extends Component {
-  constructor(props) {
-    super(props);
-    this.payWithPaystack = this.payWithPaystack.bind(this);
-    this.loadScript = this.loadScript.bind(this);
-    this.loadscriptAndUpdateState = this.loadscriptAndUpdateState.bind(this);
-    this.state = {
-      ...this.props,
-      scriptLoaded: null,
-      class: this.props.class || this.props.className || ""
-    };
-  }
 
-  componentDidMount() {
-    this.loadscriptAndUpdateState()
+function PayStack(props) {
+  const psState = {
+    ...props,
+    scriptLoaded:null,
+    class: props.class || props.className || ""
   }
+  const [payStackState,setPayStackState] = useState(psState);
 
-  loadscriptAndUpdateState() {
-    this.setState(
-      {
-        scriptLoaded: new Promise(resolve => {
-          this.loadScript(() => {
-            resolve();
-          });
-        })
-      },
-      () => {
-        if (this.props.embed) {
-          this.payWithPaystack();
-        }
+  useEffect(() => {
+       loadscriptAndUpdateState()
+      if(payStackState.scriptLoaded){
+        payWithPaystack()
       }
-    );
+  },[payStackState,loadscriptAndUpdateState,payWithPaystack])
+
+  const payWithPaystack = () => {
+    payStackState.scriptLoaded &&
+    payStackState.scriptLoaded.then(() => {
+        console.log('ref:', payStackState.reference)
+        let paystackOptions = {
+          key: payStackState.paystackkey,
+          email: payStackState.email,
+          amount: payStackState.amount,
+          ref: payStackState.reference,
+          metadata: payStackState.metadata || {},
+          callback: response => {
+            payStackState.callback(response);
+          },
+          onClose: () => {
+            payStackState.close();
+          },
+          currency: payStackState.currency,
+          plan: payStackState.plan || "",
+          quantity: payStackState.quantity || "",
+          subaccount: payStackState.subaccount || "",
+          transaction_charge: payStackState.transaction_charge || 0,
+          bearer: payStackState.bearer || "",
+        };
+        if (payStackState.embed) {
+          paystackOptions.container = "paystackEmbedContainer";
+        }
+        const handler = window.PaystackPop.setup(paystackOptions);
+        if (!payStackState.embed) {
+          handler.openIframe();
+        }
+      });
   }
 
-  loadScript(callback) {
+  const loadScript = (callback) => {
     const script = document.createElement("script");
     script.src = "https://js.paystack.co/v1/inline.js";
     document.getElementsByTagName("head")[0].appendChild(script);
@@ -58,76 +73,36 @@ class PayStack extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    for (const index in prevProps) {
-      if (prevState[index] !== this.state[index]) {
-        this.loadscriptAndUpdateState()
-      }
-    }
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    for (const index in nextProps) {
-      if (nextProps[index] !== prevState[index]) {
-        return {
-          scriptLoaded: null,
-          [index]: nextProps[index]
+  const loadscriptAndUpdateState = () => {
+    setPayStackState(
+      {...payStackState,
+        scriptLoaded: new Promise(resolve => {
+          loadScript(() => {
+            resolve();
+          });
+        })
+      },
+      () => {
+        if (props.embed) {
+          payWithPaystack();
         }
       }
-    }
-
-    return null;
+    );
   }
-
-  payWithPaystack() {
-    this.state.scriptLoaded &&
-      this.state.scriptLoaded.then(() => {
-        console.log('ref:', this.state.reference)
-        let paystackOptions = {
-          key: this.state.paystackkey,
-          email: this.state.email,
-          amount: this.state.amount,
-          ref: this.state.reference,
-          metadata: this.state.metadata || {},
-          callback: response => {
-            this.state.callback(response);
-          },
-          onClose: () => {
-            this.state.close();
-          },
-          currency: this.state.currency,
-          plan: this.state.plan || "",
-          quantity: this.state.quantity || "",
-          subaccount: this.state.subaccount || "",
-          transaction_charge: this.state.transaction_charge || 0,
-          bearer: this.state.bearer || "",
-        };
-        if (this.state.embed) {
-          paystackOptions.container = "paystackEmbedContainer";
-        }
-        const handler = window.PaystackPop.setup(paystackOptions);
-        if (!this.state.embed) {
-          handler.openIframe();
-        }
-      });
-  }
-
-  render() {
-    const CustomTag = `${this.props.tag}`;
-    return this.props.embed ? (
+    const CustomTag = `${props.tag}`;
+    return props.embed ? (
       <div id="paystackEmbedContainer" />
     ) : (
       <Fragment>
         <CustomTag
-          className={this.state.class}
-          onClick={this.payWithPaystack}
-          disabled={this.state.disabled}
+          className={payStackState.class}
+          onClick={payWithPaystack}
+          disabled={payStackState.disabled}
         >
-          {this.state.text}
+          {payStackState.text}
         </CustomTag>
       </Fragment>
     );
-  }
 }
 
 PayStack.propTypes = {
