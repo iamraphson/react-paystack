472643542fb235fd314c4702e30f4860c003d6ea
tge24 <thankgodegbo@gmail.com>
2020-06-12 09:33:11 +0100

    Removed console.log from the paystack-actions.ts file

--- libs/paystack-actions.ts
+++ libs/paystack-actions.ts
@@ -1,6 +1,5 @@
 export const callPaystackPop = (paystackArgs: Record<string, any>): void => {
-    console.log('callPaystackPop', paystackArgs);
-    //@ts-ignore
-    const handler = window.PaystackPop && window.PaystackPop.setup(paystackArgs);
-    handler && handler.openIframe();
+	//@ts-ignore
+	const handler = window.PaystackPop && window.PaystackPop.setup(paystackArgs);
+	handler && handler.openIframe();
 };
